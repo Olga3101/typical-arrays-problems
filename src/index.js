5e01f274356958efb4af4bb6d58af1e9f77adc18
Olga3101 <inozemtsevaoa@mail.ru>
2021-03-15 16:30:18 +0400

    typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) 
+{
+  if ( array == undefined )
+    return 0;
+
+  let n = array.length;
+  if ( n == 0 )
+    return 0;
+
+  let i;
+  let minimum = Infinity;
+
+   for ( i = 0; i < n; i++ ) 
+   {
+     if ( array[i] < minimum )
+       minimum = array[i];
+   }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array == undefined )
+    return 0;
+
+  let n = array.length;
+  if ( n == 0 )
+    return 0;
+
+  let i;
+  let maximum = -Infinity;
+
+   for ( i = 0; i < n; i++ ) 
+   {
+     if ( array[i] > maximum )
+      maximum = array[i];
+   }   
+   return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array == undefined )
+    return 0;
+
+  let n = array.length;
+  if ( n == 0 )
+    return 0;
+
+  let i;
+  let av = 0;
+
+  for ( i = 0; i < n; i++ ) 
+  {
+    av = av + array[i];
+  }
+  av = av / n;
+  return av;
 }
